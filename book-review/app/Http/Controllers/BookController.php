4ac0de1084b462432c58bf5cc4dcb3a28432cbb9
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-08 22:35:56 +0530

    One Page Book 'Book-Review'

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -12,13 +12,24 @@ class BookController extends Controller
      */
     public function index(Request $request)
     {
-        $title = $request->input("title");
+        $title = $request->input('title');
+        $filter = $request->input('filter', '');
+        
+        $books = Book::when(
+            $title, 
+            fn($query, $title) => $query->title($title));
+        
+        $books = match($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
 
-        $books = Book::when($title, fn($query, $title) =>
-             $query->title($title))
-            ->get();
+        $books = $books->get();
 
-            return view('books.index',['books' => $books]);
+        return view('books.index',['books' => $books]);
     }
 
     /**
@@ -40,9 +51,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view(
+            'books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -4,11 +4,30 @@
     <h1 class="mb-10 text-2xl">Books</h1>
 
     <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
-        <input type="text" name="title" placeholder="Search by title"
-            value="{{ request('title') }}" class="input h-10"/>
+        <input type="text" name="title" placeholder="Search by title" value="{{ request('title') }}" class="input h-10" />
+        <input type="hidden" name="filter" value="{{ request('filter') }}" />
         <button type="submit" class="btn">Search</button>
         <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
     </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular Last Month',
+                'popular_last_6months' => 'Popular Last 6 Months',
+                'highest_rated_last_month' => 'Highest Rated Last Month',
+                'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $label)
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+                class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
+                {{ $label }}
+            </a>
+        @endforeach
+    </div>
 
     <ul>
         @forelse ($books as $book)
@@ -16,7 +35,7 @@
                 <div class="book-item">
                     <div class="flex flex-wrap items-center justify-between">
                         <div class="w-full flex-grow sm:w-auto">
-                            <a href="{{ route('books.show', $book)}}" class="book-title">{{ $book->title }}</a>
+                            <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
                             <span class="book-author">by {{ $book->author }}</span>
                         </div>
                         <div>
@@ -24,7 +43,7 @@
                                 {{ number_format($book->reviews_avg_rating, 1) }}
                             </div>
                             <div class="book-review-count">
-                                out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count )}}
+                                out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
                             </div>
                         </div>
                     </div>
