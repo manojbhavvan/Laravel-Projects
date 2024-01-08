af1aff513d46019b2f7069ea416663df33ea2044
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-08 12:47:08 +0530

    Added scopedQuery functions 'Book-Review'

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,38 @@ class Book extends Model
 
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title){
+        return $query->where('title','LIKE','%'. $title .'%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder{
+        return $query->withCount(['reviews'=> fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderBy('reviews_count','desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder{
+        return $query->withAvg(
+            ['reviews'=> fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ],'rating')
+            ->orderBy('reviews_avg_rating','desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder{
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null){
+        if($from && !$to){
+            $query->where('created_at', '>=' ,$from);
+        }
+        elseif(!$from && $to){
+            $query->where('created_at', '<=', $to);
+        }
+        elseif($from && $to){
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
