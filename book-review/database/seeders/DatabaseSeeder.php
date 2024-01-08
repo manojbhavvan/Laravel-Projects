0ce4bde5b063c45e23ca0d6e82eeee93e136f211
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-08 11:48:04 +0530

    Factory and Seeders 'Book-Review'

--- book-review/database/seeders/DatabaseSeeder.php
+++ book-review/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Review;
+use App\Models\Book;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,32 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Book::factory(33)->create()->each(function ($book){
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+                ->good()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(33)->create()->each(function ($book){
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+                ->average()
+                ->for($book)
+                ->create();
+        });
+
+        Book::factory(34)->create()->each(function ($book){
+            $numReviews = random_int(5,30);
+
+            Review::factory()->count($numReviews)
+                ->bad()
+                ->for($book)
+                ->create();
+        });
     }
 }
