d9bd0d44f7207d1d2ba20f5a902a0ae532afe50d
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-10 00:30:16 +0530

    Throttle API 'Event-Management'

--- Event-Management/app/Http/Controllers/Api/EventController.php
+++ Event-Management/app/Http/Controllers/Api/EventController.php
@@ -20,6 +20,8 @@ class EventController extends Controller
 
     public function __construct(){
         $this->middleware('auth:sanctum')->except(['index','show']);
+        $this->middleware('throttle:api')
+            ->only(['store','update','destroy']);
         $this->authorizeResource(Event::class, 'event');
     }
 
