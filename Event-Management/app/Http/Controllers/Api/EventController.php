86f0293112e4d1052f2039f7d32a275c3a32f642
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-09 23:18:15 +0530

    Token-Auth Sanctum Added 'Event-Management'

--- Event-Management/app/Http/Controllers/Api/EventController.php
+++ Event-Management/app/Http/Controllers/Api/EventController.php
@@ -16,6 +16,11 @@ class EventController extends Controller
     /**
      * Display a listing of the resource.
      */
+
+    public function __construct(){
+        $this->middleware('auth:sanctum')->except(['index','show']);
+    }
+
     public function index()
     {
         
@@ -41,7 +46,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
         return new EventResource($this->loadRelationships($event));
--- Event-Management/routes/api.php
+++ Event-Management/routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AttendeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,10 @@ use App\Http\Controllers\Api\EventController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class );
 Route::apiResource('events.attendees', AttendeeController::class)
