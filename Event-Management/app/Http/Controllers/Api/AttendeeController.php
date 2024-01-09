d0ab0d2dc0faf1cb77f60e2170dd97fb675dee40
Manoj Bhavvan <manojbhavvan124@gmail.com>
2024-01-09 22:27:27 +0530

    Pagination and Relations Functionalities Added 'Event-Management'

--- Event-Management/app/Http/Controllers/Api/AttendeeController.php
+++ Event-Management/app/Http/Controllers/Api/AttendeeController.php
@@ -2,33 +2,55 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Event;
+use App\Models\Attendee;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 
 class AttendeeController extends Controller
 {
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event)
     {
-        //
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
+        );
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
-        //
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
+        );
+
+        return new AttendeeResource($attendee);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event, Attendee $attendee)
     {
-        //
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
     }
 
     /**
@@ -42,8 +64,10 @@ class AttendeeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $event, Attendee $attendee)
     {
-        //
+        $attendee->delete();
+
+        return response(status : 204);
     }
 }
--- Event-Management/app/Http/Controllers/Api/EventController.php
+++ Event-Management/app/Http/Controllers/Api/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,14 +10,25 @@ use App\Http\Resources\EventResource;
 
 class EventController extends Controller
 {
+    use CanLoadRelationships;
+
+    private array $relations =['user','attendees','attendees.user'];
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return EventResource::collection(Event::with('user')->get());
+        
+        $query = $this->loadRelationships(Event::query());
+        
+
+        //$this->shouldIncludeRelation('user');
+        return EventResource::collection(
+            $query->latest()->paginate()
+        );
     }
 
+    
     /**
      * Store a newly created resource in storage.
      */
@@ -32,7 +44,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -40,8 +52,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        $event->load('user', 'attendees');
-        return new EventResource($event);
+        // $event->load('user', 'attendees');
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
@@ -58,7 +70,7 @@ class EventController extends Controller
             ])
         );
 
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
     }
 
     /**
